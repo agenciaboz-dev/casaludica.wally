ba1bba9f031e12d5f08eef52b72cbaabf077b69a
ispadilha <israel.padilha@gmail.com>
2024-03-18 12:01:07 -0300

    update: results context interfaces

--- src/contexts/resultsContext.tsx
+++ src/contexts/resultsContext.tsx
@@ -1,16 +1,17 @@
 import { createContext, useState } from "react"
 import React from "react"
 
-export interface Results {
-    stage1_elapsed_time: string
-    stage1_errors: number
-
-    stage2_elapsed_time: string
-    stage2_errors: number
-
-    stage3_elapsed_time: string
-    stage3_errors: number
+interface Result {
+    elapsed_time: string
+    errors: number
 }
+
+export interface Results {
+    1: Result
+    2: Result
+    3: Result
+}
+
 
 interface ResultsContextValue {
     results: Results
@@ -27,14 +28,18 @@ export default ResultsContext
 
 export const ResultsProvider: React.FC<ResultsProviderProps> = ({ children }) => {
     const [results, setResults] = useState<Results>({
-        stage1_elapsed_time: "00:00",
-        stage1_errors: 0,
-
-        stage2_elapsed_time: "00:00",
-        stage2_errors: 0,
-
-        stage3_elapsed_time: "00:00",
-        stage3_errors: 0,
+        "1": {
+            elapsed_time: "",
+            errors: 0,
+        },
+        "2": {
+            elapsed_time: "",
+            errors: 0,
+        },
+        "3": {
+            elapsed_time: "",
+            errors: 0,
+        },
     })
 
     return <ResultsContext.Provider value={{ results, setResults }}>{children}</ResultsContext.Provider>
