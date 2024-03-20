4e2a52f8c3c5ec869523a5697bae14072c73c522
ispadilha <israel.padilha@gmail.com>
2024-03-20 10:50:37 -0300

    Revert "test: home bg animation"
    This reverts commit 43d7cf72e5a8565bc7351e1bb0a5803ad52123f3.

--- src/screens/Home/HomeBG.tsx
+++ src/screens/Home/HomeBG.tsx
@@ -1,26 +1,9 @@
-import React, { useEffect } from "react"
+import React from "react"
 import { View, Image, ImageBackground } from "react-native"
-import Animated, { useSharedValue, useAnimatedStyle, withRepeat, withSequence, withTiming } from "react-native-reanimated"
 
 interface HomeBGProps {}
 
 export const HomeBG: React.FC<HomeBGProps> = ({}) => {
-    const rotation = useSharedValue(0)
-
-    useEffect(() => {
-        rotation.value = withRepeat(
-            withSequence(withTiming(3, { duration: 1000 }), withTiming(-3, { duration: 2000 }), withTiming(0, { duration: 1000 })),
-            -1, // Repeat indefinitely
-            false // Do not reverse
-        )
-    }, [rotation])
-
-    const animatedStyle = useAnimatedStyle(() => {
-        return {
-            transform: [{ rotate: `${rotation.value}deg` }],
-        }
-    })
-
     return (
         <View style={{ flex: 1, position: "absolute", zIndex: -1 }}>
             <View
@@ -30,18 +13,9 @@ export const HomeBG: React.FC<HomeBGProps> = ({}) => {
                 source={require("../../../assets/game/1/background/1.webp")}
                 style={{ resizeMode: "cover", aspectRatio: 1, position: "absolute", left: -220, top: -450, width: 450, height: 900, zIndex: -2 }}
             />
-            <Animated.Image
+            <Image
                 source={require("../../../assets/interface/ludica_1.webp")}
-                style={[
-                    {
-                        width: 215,
-                        height: 250,
-                        position: "absolute",
-                        right: 50,
-                        bottom: 50,
-                    },
-                    animatedStyle,
-                ]}
+                style={{ width: 215, height: 250, position: "absolute", right: 50, bottom: 50 }}
             />
             <Image
                 source={require("../../../assets/interface/pilhantra_resultado_2.webp")}
