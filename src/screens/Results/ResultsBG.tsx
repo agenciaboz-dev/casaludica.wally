55e18ed475db38019144684685e12e835d9a4737
ispadilha <israel.padilha@gmail.com>
2024-03-21 11:06:32 -0300

    style: results speech balloon

--- src/screens/Results/ResultsBG.tsx
+++ src/screens/Results/ResultsBG.tsx
@@ -1,5 +1,7 @@
 import React from "react"
-import { View, Image } from "react-native"
+import { View, Image, Text } from "react-native"
+import { textStyle } from "../../style/textStyle"
+import { colors } from "../../style/colors"
 
 interface ResultsBGProps {}
 
@@ -15,8 +17,35 @@ export const ResultsBG: React.FC<ResultsBGProps> = ({}) => {
             />
             <Image
                 source={require("../../../assets/interface/ludica_2.webp")}
-                style={{ width: 250, height: 300, position: "absolute", right: 20, bottom: -420, resizeMode: "contain" }}
+                style={{ width: 240, height: 280, position: "absolute", right: 25, bottom: -420, resizeMode: "contain" }}
             />
+            <View style={{ position: "absolute", bottom: -250, left: -60 }}>
+                <Image
+                    source={require("../../../assets/interface/balao.webp")}
+                    style={{ width: 260, height: 180, resizeMode: "contain", zIndex: -1 }}
+                />
+                <View
+                    style={{
+                        position: "absolute",
+                        width: 260,
+                        height: 165,
+                        justifyContent: "center",
+                        alignItems: "center",
+                    }}
+                >
+                    <Text
+                        style={{
+                            ...textStyle,
+                            color: colors.blue,
+                            fontSize: 15,
+                            textAlign: "center",
+                            width: 180,
+                        }}
+                    >
+                        Mandou bem! Jogue novamente se acha que consegue fazer melhor!
+                    </Text>
+                </View>
+            </View>
             <Image
                 source={require("../../../assets/interface/ovos.webp")}
                 style={{ width: 150, height: 200, position: "absolute", right: -250, bottom: -420, resizeMode: "contain" }}
--- src/screens/Results/index.tsx
+++ src/screens/Results/index.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { View, Image, TouchableOpacity, Text } from "react-native"
+import { View, TouchableOpacity, Text } from "react-native"
 import { NavigationProp } from "@react-navigation/native"
 import { colors } from "../../style/colors"
 import { ResultsBG } from "./ResultsBG"
@@ -42,14 +42,6 @@ export const ResultsPage: React.FC<ResultsPageProps> = ({ navigation }) => {
                 <StageResult scene="Tarde" stage={2} />
                 <StageResult scene="Noite" stage={3} />
             </View>
-
-            <Image
-                source={require("../../../assets/interface/balao.webp")}
-                style={{ width: 270, height: 180, bottom: 160, right: 0, resizeMode: "contain", position: "absolute", zIndex: -1 }}
-            />
-            <Text style={{ ...textStyle, color: colors.blue, fontSize: 15, width: 180, position: "absolute", bottom: 205, right: 40 }}>
-                Mandou bem! Jogue novamente se acha que consegue fazer melhor!
-            </Text>
 
             <View style={{ position: "absolute", bottom: 30 }}>
                 <TouchableOpacity onPress={() => navigation.navigate("home")} style={{ ...buttonStyle, backgroundColor: colors.orange }}>
